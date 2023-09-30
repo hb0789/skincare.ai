8d77a2840bff1714e534ae8bd963c03e730f9b53
Hardik <hardikbhatia777@gmail.com>
2023-09-30 14:44:18 +0530

    made some changes to services

--- app/components/Numbers.tsx
+++ app/components/Numbers.tsx
@@ -14,11 +14,11 @@ export default function Numbers() {
   return (
     <>
       <div className="numbers-main">
-        <NumCard number="6" desc="Full time developers" />
-        <NumCard number="60" suffix = "+"  desc="Qualified dermatologists" />
-        <NumCard number="7" desc="Partnered healthcentres" />
-        <NumCard number="280" suffix = "+" desc="Daily online consumers" />
-        <NumCard number="98" suffix = "%" desc="Positive user reviews" />
+        <NumCard number={6} suffix = "" desc="Full time developers" />
+        <NumCard number={60} suffix = "+"  desc="Qualified dermatologists" />
+        <NumCard number={7} suffix = "" desc="Partnered healthcentres" />
+        <NumCard number={280} suffix = "+" desc="Daily online consumers" />
+        <NumCard number={98} suffix = "%" desc="Positive user reviews" />
       </div>
     </>
   );
