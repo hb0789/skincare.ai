eb2363fb77af5b7dbc1bc25e866809d3a1874eae
Hardik <hardikbhatia777@gmail.com>
2023-10-06 16:19:37 +0530

    fixed responsive design

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import { Inter } from 'next/font/google'
-import { Parallax } from 'react-parallax';
-import Background from '../media/download.png'
+import React from "react";
+import { Inter } from "next/font/google";
+import { Parallax, Background } from "react-parallax";
+import BackgroundPhoto from "../media/download.png";
 import "../Style/MainStyle.css";
+
 const inter = Inter({
-  subsets: ['latin'],
-  weight: '400'
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export default function Main() {
-  const imageUrl = Background.src;
+  const imageUrl = BackgroundPhoto.src;
   return (
-    <Parallax className='home-background' bgImage={imageUrl} strength={500}> 
-      <div className = {'poppins.className home-title'}>
-        Skincare.ai <br/>
-        <div className={'home-description'}>
-          Worried about skin issues?<br/>
-        </div><br/> 
+    <Parallax
+      className="home-background"
+      bgImage={imageUrl}
+      bgClassName="home-background"
+      strength={600}
+    >
+      <div className="home-title">
+        Skincare.ai <br />
+        <div className="home-description">
+          Worried about skin issues?
+          <br />
+        </div>
+        <br />
       </div>
-      <div className='home-button-div'>
-      <button className='home-button'>
-          Get started  →
-      </button>
+      <div className="home-button-div">
+        <button className="home-button">Get started →</button>
       </div>
     </Parallax>
-  )
+  );
 }
---
+++ app/main/main-components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { CircleStackIcon } from '@heroicons/react/20/solid'
+import { PlayCircleIcon } from '@heroicons/react/24/outline'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar-main'>
+        Skincare.ai
+        
+        <CircleStackIcon/>
+    </div>
+  )
+}
