f2412e891b6b5f9aa55e8d706aa229ff27b98c45
sa778888 <arshlaansiddiquie@gmail.com>
2023-10-07 22:18:19 +0530

    route main added

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,13 +3,14 @@ import { Inter } from "next/font/google";
 import { Parallax, Background } from "react-parallax";
 import BackgroundPhoto from "../media/download.png";
 import "../Style/MainStyle.css";
-
+import { useRouter } from "next/navigation";
 const inter = Inter({
   subsets: ["latin"],
   weight: "400",
 });
 
 export default function Main() {
+  const router=useRouter();
   const imageUrl = BackgroundPhoto.src;
   return (
     <Parallax
@@ -17,17 +18,17 @@ export default function Main() {
       bgImage={imageUrl}
       bgClassName="home-background"
       strength={600}
-    >
+    >      
       <div className="home-title">
         Skincare.ai <br />
-        <div className="home-description">
+        <div className="home-description ">
           Worried about skin issues?
           <br />
         </div>
         <br />
       </div>
       <div className="home-button-div">
-        <button className="home-button">Get started →</button>
+        <button className="home-button " onClick={e => router.push('/main')}>Get started →</button>
       </div>
     </Parallax>
   );
