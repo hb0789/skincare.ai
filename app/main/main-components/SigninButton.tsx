b04346391e6ab5a9084f3e7a1009f7cd50212f89
ARSHLAAN <83699737+sa778888@users.noreply.github.com>
2023-10-23 00:16:04 +0530

    signin responsive

--- app/main/main-components/SigninButton.tsx
+++ app/main/main-components/SigninButton.tsx
@@ -1,46 +1,45 @@
 "use client";
-
+import "../styles/Signin.css";
 import { signOut, signIn, useSession, SessionProvider } from 'next-auth/react'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import NavItem from './NavItem.jsx';
 
 const SigninButton = () => {
-    const [open, setOpen] = useState(false);
-    const {data: session} = useSession();
-    if(session && session.user)
-    {
-        return(
-            <div className='flex gap-4 ml-auto text-2xl items-center'>
-                  <NavItem image = {session.user.image}>
-                    <div className='dropdown-menu'>
-                      <div>
-                      {session.user.name}
-                      </div>
-                      <div>
-                        {session.user.email}
-                      </div>
-                      <hr/>
-                      <div>
-                        Settings
-                      </div>
-                      <div>
-                        Profile
-                      </div>
-                      <div>
-                      <button onClick={() => signOut()}>
-                        <div className='main-signout-button'>
-                          Sign out
-                        </div>
-                      </button>
-                      </div>
-                    </div>
-                  </NavItem>
+  const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
+  if (session && session.user) {
+    return (
+      <div className='flex gap-4 ml-auto text-2xl items-center'>
+        <NavItem image={session.user.image}>
+          <div className='dropdown-menu'>
+            <div>
+              {session.user.name}
             </div>
-        )
-    }
+            <div>
+              {session.user.email}
+            </div>
+            <hr />
+            <div>
+              Settings
+            </div>
+            <div>
+              Profile
+            </div>
+            <div>
+              <button onClick={() => signOut()}>
+                <div className='main-signout-button'>
+                  Sign out
+                </div>
+              </button>
+            </div>
+          </div>
+        </NavItem>
+      </div>
+    )
+  }
   return (
     <button className='ml-auto' onClick={() => signIn()}>
-      <div className='main-signin-button'>
+      <div className='button-34 main-signin-button'>
         Sign In
       </div>
     </button>
