4ef732c6821a68e3365c80ff8adfb67d0c9b4713
Hardik <hardikbhatia777@gmail.com>
2023-10-24 21:02:50 +0530

    added ML services

--- app/main/main-components/Uploader.jsx
+++ app/main/main-components/Uploader.jsx
@@ -1,31 +1,39 @@
-"use client";
-import { useState, useEffect } from "react";
-import "../styles/Main.css";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
-import axios from 'axios';
-import React from "react";
+import axios from "axios";
+import "../styles/Main.css";
 
 export default function Uploader() {
   const [file, setFile] = useState(null);
   const [image, setImage] = useState(null);
   const [fileName, setFileName] = useState("No files selected");
   const [diseaseData, setDiseaseData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const getDisease = async () => {
+    setLoading(true); // Set loading state to true
+
     const formData = new FormData();
     formData.append("file", file);
 
     try {
-      const response = await axios.post("http://localhost:8000/classify/", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      setDiseaseData(response.data); // Store the response data in state
+      const response = await axios.post(
+        "http://localhost:8000/classify/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      setDiseaseData(response.data);
       console.log(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false); // Set loading state back to false
     }
   };
 
@@ -37,47 +45,54 @@ export default function Uploader() {
           className="uploader"
           onClick={() => document.querySelector(".input-image").click()}
         >
-        <input
-                type="file"
-                accept="image/*"
-                className="input-image"
-                hidden
-                onChange={({ target: { files } }) => {
-                if (files && files[0]) {
-                    setFileName(files[0].name);
-                    setFile(files[0]);
-                    setImage(URL.createObjectURL(files[0]));
-                }
-                }}
-            />
+          <input
+            type="file"
+            accept="image/*"
+            className="input-image"
+            hidden
+            onChange={({ target: { files } }) => {
+              if (files && files[0]) {
+                setFileName(files[0].name);
+                setFile(files[0]);
+                setImage(URL.createObjectURL(files[0]));
+              }
+            }}
+          />
 
           {file ? (
-            <img src={image} height="200px" width="200px" />
+            <img src={image} height="200px" width="200px" alt="Uploaded" />
           ) : (
-            <div className="uploader-placeholder">
-                Upload here!
-            </div>
+            <div className="uploader-placeholder">Upload here!</div>
           )}
         </form>
         <section>
           <span>
             {file ? (
               <span className="uploader-button">
-                {fileName}{" "}
-                <FontAwesomeIcon
-                  onClick={() => {setFile(null); setDiseaseData(null);}}
-                  icon={faTrashCan}
-                />
-                <br/>
-                <button className="uploader-submit" onClick={getDisease}>
-                    Submit
-                </button>
+                {loading ? (
+                  <div className="loader"></div>
+                ) : (
+                  <>
+                    {fileName}{" "}
+                    <FontAwesomeIcon
+                      onClick={() => {
+                        setFile(null);
+                        setDiseaseData(null);
+                      }}
+                      icon={faTrashCan}
+                    />
+                    <br />
+                    <button className="uploader-submit" onClick={getDisease}>
+                      Submit
+                    </button>
+                  </>
+                )}
                 {diseaseData && (
-                    <div className="disease-data">
+                  <div className="disease-data">
                     <h2>Disease Classification</h2>
                     <p>{diseaseData.prediction}</p>
                     <p>{diseaseData.accuracy}</p>
-                    </div>
+                  </div>
                 )}
               </span>
             ) : (
