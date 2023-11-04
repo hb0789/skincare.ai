479b1fd2f78fd89c346b152a74850c9bf7b11b76
ARSHLAAN <83699737+sa778888@users.noreply.github.com>
2023-11-04 23:06:27 +0530

    register

--- app/main/signin/page.jsx
+++ app/main/signin/page.jsx
@@ -3,18 +3,49 @@ import '../styles/Register.css'
 import {React,useEffect,useState} from 'react';
 import { signOut, signIn, useSession, SessionProvider } from 'next-auth/react'
 import { useRouter } from "next/navigation";
+import { collection, addDoc } from 'firebase/firestore';
+import {   getDocs, query, where } from 'firebase/firestore';
+
+import { db } from '@/app/HOCS/firebase';
 
 const Register = () => {
   const router = useRouter();
   const { data: session } = useSession();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [success, setSuccess] = useState(false);
+  
+
 
   useEffect(() => {
-    if (session) {
+    if (session || success) {
       router.push("/main");
       
     }
-  }, [session,router]);
+  }, [session,success,router]);
 
+ 
+
+  const handleRegister = async () => {
+
+    const userData = {
+      credits: 100,
+      email,
+      password,
+      
+    };
+
+    try {
+      const docRef = await addDoc(collection(db, 'item'), userData);
+      console.log('User data added with ID: ', docRef.id);
+      setSuccess(true);
+    } 
+    catch (error) {
+      console.error('Error adding user data: ', error);
+    }
+    
+
+  };
   
 
   if (typeof session === "undefined") {
@@ -27,14 +58,17 @@ const Register = () => {
       
     );
   }
-  else if(!session){
+  else{
   return (
     <div className="login-container">
       <h1>Register</h1>
       <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Register</button>
+        <input type="email" placeholder="Email" value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+
+        <input type="password" placeholder="Password" value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleRegister} >Register</button>
       </form>
       
       <div className="or-divider">
@@ -51,7 +85,7 @@ const Register = () => {
         <div className='logo'>
         <img className='google-image'
           src="https://img.icons8.com/fluent/48/000000/google-logo.png"
-          alt="Google Logo" 
+          alt="Google Logo" onClick={() => signIn()}
         /></div>
       </div>
     </div>
