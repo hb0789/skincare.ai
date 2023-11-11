71d1bf71abbe42647fe173f009b1f00fb3e0a55a
ARSHLAAN <83699737+sa778888@users.noreply.github.com>
2023-11-11 16:04:02 +0530

    Revert "login using email and password, updation of profile and signin button "

--- app/main/profile/page.jsx
+++ app/main/profile/page.jsx
@@ -13,30 +13,12 @@ import {
     where,
   } from "firebase/firestore";
 import { db } from '@/app/HOCS/firebase';
-import { onAuthStateChanged } from "firebase/auth";
-import { getAuth, signOut as sg } from "firebase/auth";
-
-const auth = getAuth();
 
 const Page = () => {
     const { data: session, status } = useSession();
     const [items, setItems] = useState([]);
     const [profilePhoto, setProfilePhoto] = useState("");
-    const [authUser,setauthUser]=useState(auth.currentUser);
-    
-    
-    useEffect(()=>{
-      const listen=onAuthStateChanged(auth,(user)=>{
-        if(user){
-          setauthUser(user);
-        }
-        else{
-          setauthUser(null);
-        }
-      })
-    },[]);
-
-
+  
     useEffect(() => {
       if (status === "authenticated" && session?.user?.email) {
         const q = query(collection(db, "item"), where("email", "==", session.user.email));
@@ -49,20 +31,7 @@ const Page = () => {
           setItems(itemsArr);
         });
       }
-      else if (authUser!=null) {
-        const q = query(collection(db, "item"), where("email", "==", authUser.email));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          let itemsArr = [];
-          querySnapshot.forEach((doc) => {
-            itemsArr.push({ ...doc.data(), id: doc.id });
-          });
-          console.log(itemsArr);
-          setItems(itemsArr);
-        });
-      }
-
-    }, [status, session,authUser]);
-    
+    }, [status, session]);
   
     useEffect(() => {
       if (session?.user?.image) {
@@ -70,8 +39,7 @@ const Page = () => {
         setProfilePhoto(modifiedImage);
       }
     }, [session]);
-    
-    if(session){
+  
     return (
      
         
@@ -99,47 +67,6 @@ const Page = () => {
       
     )
   }
-  else if(authUser){
-    return (
-     
-        
-      <div className='prof app-background'>
-        <Navbar />
-        <div className='profile-left'>
-           <img className='profile-photo-main' src="../avatar.png" alt='no image uploaded'/>
-          <div className='profile-name-div'>
-            {items[0]?.name}
-          </div>
-          <div className='profile-email-div'>
-            {items[0]?.email}
-          </div>
-          {items.length > 0 ? (
-        <div className="user-info">
-          <h2>User Information</h2>
-          <p>Name: {items[0]?.name}</p>
-          <p>Email: {items[0]?.email}</p>
-          <p>Age: {items[0]?.age}</p>
-          {/* Add more fields from itemsArr as needed */}
-        </div>
-      ) : null}
-        </div>
-      </div>
-      
-    )
-  }
-  else{
-    return (
-     
-        
-      <div className='prof app-background'>
-        <Navbar />
-        <div>
-          <h1>We ran into some issue .... </h1>
-        </div>
-      </div>
-      
-    )
-  }
-  }
+  
   export default Page;
   
